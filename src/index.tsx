81fa9db4645ba9acb95cb3a4ee99b78af34a5ac2
Bharat Kashyap <bharatkashyap@outlook.com>
2022-09-24 18:28:29 +0530

    Not required

--- src/index.tsx
+++ src/index.tsx
@@ -588,7 +588,7 @@ export default class Editor extends React.Component<Props, State> {
             : { children: highlighted })}
         />
         {/* eslint-disable-next-line react/no-danger */}
-        <style type="text/css" dangerouslySetInnerHTML={{ __html: cssText }} />
+        <style dangerouslySetInnerHTML={{ __html: cssText }} />
       </div>
     );
   }
